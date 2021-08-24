32acc55deee46e64a841e141afc6f5a0150d4597
JBatsyus <jkalabina@gmail.com>
2021-08-24 15:04:22 +0200

    Аватар

--- pr2.js
+++ pr2.js
@@ -1,12 +1,17 @@
 // проверить, есть ли в локал  сторадже имя, и если оно не равно null, то мы должны его показать
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    let avatar = localStorage.getItem('avatar');
+    if (avatar != null) {
+        document.getElementById("avatar").value = avatar;
+    }
+
     let name = localStorage.getItem('name');
     if (name != null) {
         document.getElementById("author").value = name;
     }
 
-    let comment = localStorage.getItem('comment');
+    let comment = JSON.parse(localStorage.getItem('comment'));
     if (comment != null) {
         document.getElementById("comment").value = comment;
     }
@@ -14,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // добавление в див комментария
 
-function sendMessage(author, comment) {
+function sendMessage( author, comment) {
     let userCommet = document.getElementById("comment");
     let otvet = document.querySelector(".chat");
     if (userCommet.value) {
@@ -30,13 +35,17 @@ function sendMessage(author, comment) {
 function checkMessage() {
     let author = document.getElementById("author").value;
     let comment = document.getElementById("comment").value;
+    let avatar = document.getElementById("avatar").value
 
-    // проверякм, есть ли имя пользователя в локальном хранилище
+    // проверяем, есть ли имя пользователя и комментарий в локальном хранилище, если нет, то загружаем
     if (localStorage.getItem('name') == null) {
         localStorage.setItem('name', author)
     }
     if (localStorage.getItem('comment') == null) {
-        localStorage.setItem('comment', comment)
+        localStorage.setItem('comment', JSON.stringify(comment))
+    }
+    if (localStorage('avatar') == null) {
+        localStorage.setItem('avatar', avatar)
     }
 
     sendMessage(author, comment);
