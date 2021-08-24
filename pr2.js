a1ea464eaa3abdc2c8c41ca0eaea84c653118460
JBatsyus <jkalabina@gmail.com>
2021-08-24 11:35:42 +0200

    Добавление практики №3

---
+++ pr2.js
@@ -0,0 +1,43 @@
+// проверить, есть ли в локал  сторадже имя, и если оно не равно null, то мы должны его показать
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let name = localStorage.getItem('name');
+    if (name != null) {
+        document.getElementById("author").value = name;
+    }
+
+    let comment = localStorage.getItem('comment');
+    if (comment != null) {
+        document.getElementById("comment").value = comment;
+    }
+})
+
+// добавление в див комментария
+
+function sendMessage(author, comment) {
+    let userCommet = document.getElementById("comment");
+    let otvet = document.querySelector(".chat");
+    if (userCommet.value) {
+        let str = userCommet.value;
+        str = str.toLowerCase();
+        let comment = str.replaceAll(/(viagra|xxx|sex|drugs|rock'n'roll)/g, '*****');
+        otvet.innerHTML += `<span class='author'>${author}:</span><span>${comment} </span><br>`;
+    }
+    document.getElementById("comment").value = "";
+}
+
+// проверка, добавлено ли  имя пользователя в локальное хранилище
+function checkMessage() {
+    let author = document.getElementById("author").value;
+    let comment = document.getElementById("comment").value;
+
+    // проверякм, есть ли имя пользователя в локальном хранилище
+    if (localStorage.getItem('name') == null) {
+        localStorage.setItem('name', author)
+    }
+    if (localStorage.getItem('comment') == null) {
+        localStorage.setItem('comment', comment)
+    }
+
+    sendMessage(author, comment);
+}
