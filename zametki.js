0a7fcea6d5c12d7461695aca3089e27b0f566160
JBatsyus <jkalabina@gmail.com>
2021-09-01 10:41:26 +0200

    Обновление дз

--- zametki.js
+++ zametki.js
@@ -1,9 +1,4 @@
-
-
-
 // проверить, есть ли в локал  сторадже комментарий, и если оно не равно null, то мы должны его показать
-
-
 
 document.addEventListener("DOMContentLoaded", function (event) {
     let comment = JSON.parse(localStorage.getItem('comment'));
@@ -16,20 +11,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function sendMessage() {
     let comment = document.getElementById('comment');
     let notes = document.getElementById("notes");
-   let div = document.createElement('div');
-    
-  div.innerText = comment.value;
-  div.classList.add('zametka');
-  notes.appendChild(div);
-    
-  
-  comment.value = "";
+    let div = document.createElement('div');
+
+    div.innerText = comment.value;
+    div.classList.add('zametka');
+    notes.appendChild(div);
+    comment.value = "";
 }
 
 
 function checkMessage() {
-  let comment = document.getElementById("comment").value;
-    
+    let comment = document.getElementById("comment").value;
+
     // проверяем, есть ли заметка в локальном хранилище
     if (localStorage.getItem('comment') == null) {
         localStorage.setItem('comment', JSON.stringify(comment))
